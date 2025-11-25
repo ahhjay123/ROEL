7b26af21b780613060ab5463cac330add1939bce
Cof-kel <kellydelasalas203@gmail.com>
2025-11-25 21:39:48 +0800

    your message

--- app.py
+++
@@ -1,8 +0,0 @@
-from db import initialize_database, print_db_location
-from login_ui import start_login_ui
-
-if __name__ == "__main__":
-    initialize_database()
-
-    print_db_location()
-    start_login_ui()
