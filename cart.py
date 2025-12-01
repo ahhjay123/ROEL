7139866d85da10a9d16a74c20c7ef25fa0ccaf82
_simplyahjay <aranez1127@gmail.com>
2025-12-01 09:55:17 +0800

    add method

--- cart.py
+++ cart.py
@@ -4,6 +4,10 @@ class Cart:
 
     def add(self, product_name, quantity, total):
         self.items.append((product_name, quantity, total))
+    
+    def remove(self, index):
+        if 0 <= index < len(self.items):
+            del self.items[index]
 
     def get_total(self):
         return sum(item[2] for item in self.items)
